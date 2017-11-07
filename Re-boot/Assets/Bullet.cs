d6f9591573b99758216b40d11a4f902f89453914
Paul Breton <paul.ravezb@gmail.com>
2017-11-08 00:01:44 +0100

    Added base scene, added rewind for one character, need to apply it on everyone et need to fix camera problem

--- Re-boot/Assets/Bullet.cs
+++ Re-boot/Assets/Bullet.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	private void OnCollisionEnter(Collision other)
 	{
 		var hit = other.gameObject;
--- Re-boot/Assets/PlayerController.cs
+++ Re-boot/Assets/PlayerController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+public struct TimePosition
+{
+    public float deltaTime;
+    public Vector3 position;
+
+    public TimePosition(float deltaTime, Vector3 position)
+    {
+        this.deltaTime = deltaTime;
+        this.position = position;
+    }
+}
+
 public class PlayerController : NetworkBehaviour
 {
+    public const float RewindTime = 2.0f;
+
     public GameObject bulletPrefab;
 
     public Transform bulletSpawn;
 
+    public List<TimePosition> positions;
+    public float totalRegisteredMovements = 0.0f;
+
     // Use this for initialization
     void Start()
     {
+        positions = new List<TimePosition>();
     }
 
     // Update is called once per frame
@@ -20,6 +38,7 @@ public class PlayerController : NetworkBehaviour
     {
         if (!isLocalPlayer)
         {
+            RegisterPosition();
             return;
         }
 
@@ -32,6 +51,24 @@ public class PlayerController : NetworkBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CmdFire();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            CmdRewind();
+        }
+    }
+
+    [Server]
+    void RegisterPosition()
+    {
+        positions.Add(new TimePosition(Time.deltaTime, transform.position));
+        if (totalRegisteredMovements + Time.deltaTime > 20.0f)
+        {
+            totalRegisteredMovements -= positions[0].deltaTime;
+            positions.RemoveAt(0);
+
+            totalRegisteredMovements += Time.deltaTime;
         }
     }
 
@@ -54,6 +91,29 @@ public class PlayerController : NetworkBehaviour
 
         // Destroy the bullet after 2 seconds
         Destroy(bullet, 2.0f);
+    }
+
+    [Command]
+    void CmdRewind()
+    {
+        float delta = 0.0f;
+        int i = positions.Count - 1;
+        for (; i > 0 && delta <= RewindTime; --i)
+        {
+            delta += positions[i].deltaTime;
+        }
+
+        transform.position = positions[i].position;
+        RpcRewind(transform.position);
+    }
+
+    [ClientRpc]
+    void RpcRewind(Vector3 position)
+    {
+        if (isLocalPlayer)
+        {
+            transform.position = position;
+        }
     }
 
     public override void OnStartLocalPlayer()
