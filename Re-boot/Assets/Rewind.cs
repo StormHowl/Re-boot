76ba2c2a5c433ca980050b3dc60cdcc9a3c40c47
Paul Breton <paul.ravezb@gmail.com>
2017-11-17 10:33:41 +0100

    Changed preferences

---
+++ Re-boot/Assets/Rewind.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct GameState
+{
+	//public List<DestroyableItems> items;
+	public List<TimePosition> playerPositions;
+}
+
+public class Rewind : MonoBehaviour
+{
+
+	private float _globalCooldown = 4.0f;
+	private float _teamCooldown = 2.0f;
+	private float _playerCooldown = 2.0f;
+
+	private float _rewindTime = 3.0f;
+
+	private List<GameState> _states;
+	private List<GameObject> _players;
+
+	// Use this for initialization
+	void Start () {
+		_states = new List<GameState>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void RegisterPlayer(GameObject player)
+	{
+		this._players.Add(player);
+	}
+}
