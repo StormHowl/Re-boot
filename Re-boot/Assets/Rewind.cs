f4a83c2221af4f4990cf61c2d1abeffd9f7c8f77
Paul Breton <paul.ravezb@gmail.com>
2017-11-17 10:51:46 +0100

    Trying to fix merge problem

--- Re-boot/Assets/Rewind.cs
+++ Re-boot/Assets/Rewind.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< HEAD
 
 public struct GameState
 {
@@ -35,3 +36,85 @@ public class Rewind : MonoBehaviour
 		this._players.Add(player);
 	}
 }
+=======
+using UnityEngine.Networking;
+
+public struct GameFlash
+{
+    public float deltaTime;
+    public Dictionary<GameObject, Vector3> objects;
+
+    public GameFlash(float deltaTime)
+    {
+        this.deltaTime = deltaTime;
+        this.objects = new Dictionary<GameObject, Vector3>();
+    }
+
+    public void AddObjectTimePosition(GameObject obj, Vector3 position)
+    {
+        this.objects.Add(obj, position);
+    }
+}
+
+public struct TimeObjectPosition
+{
+    public GameObject obj;
+    public Vector3 position;
+}
+
+public class Rewind : NetworkBehaviour
+{
+    private List<GameFlash> _flashes;
+    private const float SavedTimeLimit = 50.0f;
+    private float _totalSavedTime;
+
+    // Use this for initialization
+    [Server]
+    void Start()
+    {
+        if (isServer)
+        {
+            _flashes = new List<GameFlash>();
+            _totalSavedTime = 0.0f;
+        }
+    }
+
+    // Update is called once per frame
+    [Server]
+    void Update()
+    {
+        if (isServer)
+        {
+            CreateGameFlash();
+        }
+    }
+
+    [Server]
+    private void CreateGameFlash()
+    {
+        GameFlash flash = new GameFlash(Time.deltaTime);
+        foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            flash.AddObjectTimePosition(player, player.transform.position);
+        }
+        _flashes.Add(flash);
+        _totalSavedTime += flash.deltaTime;
+
+        if (!(_totalSavedTime > SavedTimeLimit)) return;
+
+        _totalSavedTime -= _flashes[0].deltaTime;
+        _flashes.RemoveAt(0);
+    }
+
+    [Command]
+    public void CmdDoRewind()
+    {
+        RpcRewindTo(_flashes[0]);
+    }
+
+    [ClientRpc]
+    void RpcRewindTo(GameFlash flash)
+    {
+    }
+}
+>>>>>>> 3d25ac42eb1e9ca5b00da569821f3dcfd4f98c60
